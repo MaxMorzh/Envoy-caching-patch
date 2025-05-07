6f4e6fb79fa44dcf45344282d19a0bd9c01c764c
MaxMorzh <maxmorzhov777@gmail.com>
2025-05-07 22:35:32 +0200

    Envoy patch

---
+++ flask_app/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import time
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    time.sleep(5)
+    return "backend reached"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
